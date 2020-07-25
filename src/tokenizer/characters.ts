a4438cdb2dd98005d87a7e3f35f501d3610c0641
William Welling <wwelling@outlook.com>
2020-07-25 18:26:13 -0500

    required characters and chracter methods

--- src/tokenizer/characters.ts
+++ src/tokenizer/characters.ts
@@ -1,9 +1,99 @@
-const ampersand = '\u0026';
-const lessThanSign = '\u003C';
-const nullCharacter = '\u0000';
+const characters: { [name: string]: string } = {
+  nullCharacter: '\0000',        //
+  characterTabulation: '\u0009', //
+  lineFeed: '\u000A',            //
+  formFeed: '\u000C',            //
+  space: '\u0020',               // ' '
+  exclamationMark: '\u0021',     // '!'
+  quotationMark: '\u0022',       // '"'
+  numberSign: '\u0023',          // '#'
+  apostrophe: '\u0027',          // '''
+  ampersand: '\u0026',           // '&'
+  hyphenMinu: '\u002D',          // '-'
+  solidus: '\u002F',             // '/'
+  semiColon: '\u003B',           // ';'
+  lessThanSign: '\u003C',        // '<'
+  equalsSign: '\u003D',          // '='
+  greaterThanSign: '\u003E',     // '>'
+  questionMark: '\u003F',        // '?'
+  latinCapitalLetterX: '\u0058', // 
+  rightSquareBracket: '\u005D',  // ']'
+  graveAccent: '\u0060',         // '`'
+  latinSmallLetterX: '\u0078',   //
+}
+
+const isASCIICaseInsensitiveMatch = (value: string, match: string): boolean => {
+  return value.toLowerCase() === match.toLowerCase();
+}
+
+const isASCIIAlpha = (character: string): boolean => {
+  const code = toCharCode(character);
+  return isCharCodeUpperAlpha(code) || isCharCodeLowerAlpha(code);
+}
+
+const isASCIIUpperAlpha = (character: string): boolean => {
+  return isCharCodeUpperAlpha(toCharCode(character));
+}
+
+const isCharCodeUpperAlpha = (code: number): boolean => {
+  return (code > 64 && code < 91); // upper alpha (A-Z)
+}
+
+const isASCIILowerAlpha = (character: string): boolean => {
+  return isCharCodeLowerAlpha(toCharCode(character));
+}
+
+const isCharCodeLowerAlpha = (code: number): boolean => {
+  return (code > 96 && code < 123); // lower alpha (a-z)
+}
+
+const isASCIIDigit = (character: string): boolean => {
+  return isCharCodeDigit(toCharCode(character));
+}
+
+const isCharCodeDigit = (code: number): boolean => {
+  return (code > 47 && code < 58); // numeric (0-9)
+}
+
+const isASCIIAlphanumeric = (character: string): boolean => {
+  const code = toCharCode(character);
+  return isCharCodeUpperAlpha(code) || isCharCodeLowerAlpha(code) || isCharCodeDigit(code);
+}
+
+const isASCIIHexDigit = (character: string): boolean => {
+  const code = toCharCode(character);
+  return isCharCodeUpperHexDigit(code) || isCharCodeLowerHexDigit(code);
+}
+
+const isASCIIUpperHexDigit = (character: string): boolean => {
+  return isCharCodeUpperHexDigit(toCharCode(character));
+}
+
+const isCharCodeUpperHexDigit = (code: number): boolean => {
+  return (code > 64 && code < 71); // upper alpha (A-F)
+}
+
+const isASCIILowerHexDigit = (character: string): boolean => {
+  return isCharCodeLowerHexDigit(toCharCode(character));
+}
+
+const isCharCodeLowerHexDigit = (code: number): boolean => {
+  return (code > 96 && code < 103); // lower alpha (a-f)
+}
+
+const toCharCode = (character: string): number => {
+  return character.charCodeAt(0);
+}
 
 export {
-  ampersand,
-  lessThanSign,
-  nullCharacter
+  characters,
+  isASCIIAlpha,
+  isASCIIUpperAlpha,
+  isASCIILowerAlpha,
+  isASCIIDigit,
+  isASCIIAlphanumeric,
+  isASCIICaseInsensitiveMatch,
+  isASCIIHexDigit,
+  isASCIIUpperHexDigit,
+  isASCIILowerHexDigit
 };
--- src/tokenizer/state/data-state.ts
+++ src/tokenizer/state/data-state.ts
@@ -1,4 +1,4 @@
-import { ampersand, lessThanSign, nullCharacter } from "../characters";
+import { characters } from "../characters";
 import { AbstractState } from "./abstract-state";
 
 // 12.2.5.1 Data state
@@ -16,11 +16,11 @@ import { AbstractState } from "./abstract-state";
 export class DataState extends AbstractState {
   consume(character: string): void {
     switch (character) {
-      case ampersand:
+      case characters.ampersand:
         break;
-      case lessThanSign:
+      case characters.lessThanSign:
         break;
-      case nullCharacter: // This is an unexpected-null-character parse error
+      case characters.nullCharacter: // This is an unexpected-null-character parse error
       default:
         break;
     }
